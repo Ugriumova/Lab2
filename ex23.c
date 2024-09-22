e565086efb1562cc2ea3672307f94a2c8823e69f
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-22 19:53:42 +0300

    Add files via upload

---
+++ ex23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+
+float main()
+{
+    int n, L;
+    float p, j;
+    setlocale(LC_CTYPE, "RUS");
+    n = 2;
+    L = 133;
+    p = 2;
+    j = 133;
+    printf("Дано:\n\t\t%d\n\t\t%d\n\t\t________\nОтвет:\n\t\t%+d%2.4f", n, L, n / L,p/j);
+
+
+}
